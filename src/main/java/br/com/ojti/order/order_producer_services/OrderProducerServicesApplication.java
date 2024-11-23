d73b8cccb5e03d067967b472c87658788462e7a7
olavoalves <olavoalvesneto@gmail.com>
2024-11-23 19:05:26 -0300

    Initial commit

---
+++ src/main/java/br/com/ojti/order/order_producer_services/OrderProducerServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.ojti.order.order_producer_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderProducerServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderProducerServicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ojti/order/order_producer_services/OrderProducerServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ojti.order.order_producer_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderProducerServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
